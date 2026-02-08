a494c0e063e6a6792430b3c41fada266e9e00fe6
chinmoysenapoti <chinmoysenapoti77x@gmail.com>
2026-02-08 18:20:31 +0530

    try-catch

--- backend/modules/servers/server.controller.ts
+++ backend/modules/servers/server.controller.ts
@@ -7,20 +7,24 @@ import {
 export async function createServerController(req: Request, res: Response) {
   try {
     const { name, avatar } = req.body;
-  const userId = req.user.userId;
+    const userId = req.user.userId;
 
-  const server = await createServerService({ name, avatar, userId });
+    const server = await createServerService({ name, avatar, userId });
 
-  res.json(server);
+    res.json(server);
   } catch (error: any) {
     res.status(400).json({ message: error.message });
   }
 }
 
 export async function listServersController(req: Request, res: Response) {
-  const userId = req.user.userId;
+  try {
+    const userId = req.user.userId;
 
-  const servers = await listUserServersService(userId);
+    const servers = await listUserServersService(userId);
 
-  res.json(servers);
+    res.json(servers);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
 }
